dc28ee66359b144e83cddc0c1a2dc320c7a3de0c
liaolinyan2018 <linyanliao@126.com>
2018-12-14 17:22:16 +0800

    修了点小bug

--- main.js
+++ main.js
@@ -3,13 +3,12 @@ var n = 1;
 $(`.images > img:nth-child(${n})`).addClass('current').siblings().addClass('enter');
 //console.log($(`.images > img:nth-child(${n})`)) //这句代码才是打出指定的img
 //console.log($('.images > img:nth-child(n)'))//这句话会打出所有img
-//console.log()
 /*两个不同状态切换间隔时间为2s*/
 setInterval(() =>{
-  $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
-    .one('transitionend',(e) => {$(e.currentTarget).removeClass('leave').addClass('enter');
+  $(`.images > img:nth-child(${x(n)})`).removeClass('current enter').addClass('leave')
+    .one('transitionend',(e) => {$(e.currentTarget).removeClass('leave current').addClass('enter');
       });//先离开，然后回去排队
-  $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current');
+  $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter leave').addClass('current');
   n++;
   },2000)
 
