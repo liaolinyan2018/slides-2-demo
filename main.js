6e94d1fa220d8d8a5ec4117c0781e2cb5b2bd4f4
liaolinyan2018 <linyan.lao@gmail.com>
2019-03-19 10:28:45 +0800

    优化无缝轮播

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
-/*初始化*/
 var n = 1;
+
 $(`.images > img:nth-child(${n})`).addClass('current').siblings().addClass('enter');
-//console.log($(`.images > img:nth-child(${n})`)) //这句代码才是打出指定的img
-//console.log($('.images > img:nth-child(n)'))//这句话会打出所有img
-/*两个不同状态切换间隔时间为2s*/
+
 setInterval(() =>{
-  $(`.images > img:nth-child(${x(n)})`).removeClass('current enter').addClass('leave')
+  $(`.images > img:nth-child(${handleN(n)})`).removeClass('current enter').addClass('leave')
     .one('transitionend',(e) => {$(e.currentTarget).removeClass('leave current').addClass('enter');
-      });//先离开，然后回去排队
-  $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter leave').addClass('current');
-  n++;
+      });//先离开，然后马上回去排队
+  $(`.images > img:nth-child(${handleN(n+1)})`).removeClass('enter leave').addClass('current');
+  n+=1;
   },2000)
 
-function x(n){ //1
+function handleN(n){ //1 2 3 4
   if(n>4) {
     n=n%4; //1 2 3 0
     if(n===0){
